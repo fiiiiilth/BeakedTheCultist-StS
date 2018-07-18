285d59c1b2920e39ac330d097fd6a1960ef4a5f3
fiiiiilth <fiiiiilth@gmail.com>
2018-07-18 14:12:56 -0400

    Added LibraryTypeEnum

---
+++ src/main/java/beaked/patches/LibraryTypeEnum.java
@@ -0,0 +1,10 @@
+package beaked.patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
+
+public class LibraryTypeEnum {
+
+    @SpireEnum
+    public static CardLibrary.LibraryType BEAKED_YELLOW;
+}
